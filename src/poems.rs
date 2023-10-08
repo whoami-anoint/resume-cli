d905950b3905af4afcf5d31c0ea93bf541beb77e
whoami-anoint <abhishekkfl@gmail.com>
2023-10-08 22:49:32 +0530

    update poems.rs

---
+++ src/poems.rs
@@ -0,0 +1,14 @@
+use colored::Colorize;
+
+pub fn show_poems() {
+    println!("");
+    println!("You can contact me at:");
+    println!("Burpsuite: {}" , "https://anoint.ninja/burpsuite/".bright_green());
+    println!("cURL: {}" , "https://anoint.ninja/".bright_green());
+    println!("Nmap: {}" , "https://anoint.ninja/".bright_green());
+    println!("Wireshark: {}" , "https://anoint.ninja/".bright_green());
+    println!("reNgine: {}" , "https://anoint.ninja/".bright_green());
+    println!("Python: {}" , "https://anoint.ninja/".bright_green());
+    println!("Golang: {}" , "https://anoint.ninja/".bright_green());
+    println!("");
+}
